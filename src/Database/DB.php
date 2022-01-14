ddc8a20a042a2a6b0be0eb4aba29742b6e07ef72
cesaredifoggia98 <cesaredifoggiadev@gmail.com>
2022-01-14 01:00:17 +0100

    framework first commit

---
+++ src/Database/DB.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Database;
+
+use PDO;
+
+class DB {
+    private static $currentInstance = null;
+
+    private $selects = ['*'];
+    private $table = '';
+    private $wheres = [];
+    private $model = null;
+
+    private static function getInstance() {
+        if (self::$currentInstance === null) {
+            self::$currentInstance = new DB;
+        }
+
+        return self::$currentInstance;
+    }
+
+    //SELECT * FROM table WHERE id = ? OR WHERE id > 0 
+    protected function buildQuery() {
+        $selectStmt = implode(',', $this->selects);
+        $whereValues = [];
+        $whereStmts = [];
+        foreach ($this->wheres as $index => $where) {
+            $stmtString = '';
+            if ($index > 0) {
+                $stmtString .= ' ' .$where['boolean'] .' ';
+            } else {
+                $stmtString .= ' WHERE ';
+            }
+            $stmtString .= $where['column'] .' ' .$where['operator'] .' ?';
+            $whereValues[] = $where['value'];
+            $whereStmts[] = $stmtString;
+        }
+        $wheresStmt = implode(' ', $whereStmts);
+        $sql = "SELECT $selectStmt FROM {$this->table}{$wheresStmt}";
+
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute($whereValues);
+
+        return $stmt;
+    }
+
+    private function connect() {
+        $config = include('./config.php');
+        $dbConfig = $config['db'];
+
+        $driver = $dbConfig['driver'];
+        $host = $dbConfig['host'];
+        $port = $dbConfig['port'];
+        $db = $dbConfig['db'];
+        $username = $dbConfig['username'];
+        $password = $dbConfig['password'];
+        $connectionString = "$driver:host=$host:$port;dbname=$db";
+
+        return new PDO($connectionString, $username, $password, $dbConfig['options']);
+    }
+
+    public function first() {
+        $stmt = $this->buildQuery();
+
+        $result = $stmt->fetch();
+
+        if ($result) {
+            if ($this->model) {
+                $modelClass = $this->model;
+                return $modelClass::create($result);
+            }
+    
+            return $result;
+        }
+
+        return null;
+    }
+
+    public function last() {
+        
+    }
+
+    public function get() {
+        $stmt = $this->buildQuery();
+
+        $results = $stmt->fetchAll();
+
+        if ($this->model) {
+            $modelClass = $this->model;
+            return $modelClass::from($results);
+        }
+        return $results;
+    }
+
+    public function addSelect($column) {
+        if (is_array($column)) {
+            $this->selects = $column;
+        } else {
+            if (count($this->selects) == 1 && $this->selects[0] == '*') {
+                $this->selects = [$column];
+            } else {
+                $this->selects[] = $column;
+            }
+        }
+
+        return $this;
+    }
+
+    public function addModel($modelClass) {
+        $this->model = $modelClass;
+
+        return $this;
+    }
+
+    public function addTable($table) {
+        $this->table = $table;
+
+        return $this;
+    }
+    
+    public function addWhere($column, $operator = '=', $value = '', $boolean = 'AND') {
+        $this->wheres[] = ['column' => $column, 'operator' => $operator, 'value' => $value, 'boolean' => $boolean];
+
+        return $this;
+    }
+
+    public static function __callStatic($name, $arguments) {
+        return self::resolveName($name, $arguments);
+    }
+
+    public function __call($name, $arguments) {
+        return self::resolveName($name, $arguments);
+    }
+
+    private static function resolveName($name, $arguments) {
+        $completeName = '';
+        $additionalArgs = [];
+
+        if ($name == 'select' || $name == 'table' || $name == 'where' || $name == 'model') {
+            $completeName = 'add' .ucfirst($name);
+        } else if (str_starts_with($name, 'or')) {
+            $completeName = 'add' .ucfirst(strtolower(str_replace('or', '', $name)));
+            $additionalArgs[] = 'OR';
+        }
+        if ($completeName) {
+            return self::getInstance()->$completeName(...$arguments, ...$additionalArgs);
+        }
+    }
+
+}
---
+++ src/Model.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Models;
+
+use Database\DB;
+
+abstract class Model {
+    private static $currentBuilder;
+    protected $attributes = [];
+
+    protected $table = '';
+
+    public static function all() {
+        $emptyModel = new static;
+        return static::from(DB::table($emptyModel->table)->get());
+    }
+
+    public static function from($modelsArray = []) {
+        $models = []; 
+        foreach ($modelsArray as $attributes) {
+            $models[] = static::create($attributes);
+        }
+
+        return $models;
+    }
+
+    public static function create($attributes = []) {
+        return new static($attributes);
+    }
+
+    public function __construct($attributes = []) {
+        $this->attributes = $attributes;
+    }
+
+    public function __get($name) {
+        if (isset($this->attributes[$name])) {
+            return $this->attributes[$name];
+        }
+        $methodName = 'get' .ucfirst($name) .'Attribute';
+        if (is_callable([$this, $methodName])) {
+            return $this->$methodName();
+        }
+
+        return null;
+    }
+
+    public static function __callStatic($name, $arguments) {
+        if (!static::$currentBuilder) {
+            $emptyModel = new static;
+            static::$currentBuilder = DB::model(static::class)->table($emptyModel->table);
+        }
+        if (is_callable([static::$currentBuilder, $name])) {
+            return static::$currentBuilder->$name(...$arguments); 
+        }
+    }
+
+}
